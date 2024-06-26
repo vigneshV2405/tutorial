8c7822fe1f2cee08c18393e8d980eea9234aaad0
vigneshV2405 <vigneshvarikolu133@gmail.com>
2024-06-26 12:16:35 +0530

    todos

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Todolist from "./components/Todolist.svelte";
+
+  
+</script>
+<main>
+    <h1>Good Morning!!</h1>
+    <Todolist/>
+</main>
---
+++ src/components/Todolist.svelte
@@ -0,0 +1,36 @@
+<script>
+    let todos = ['Good Morning!','Vignesh','Hello']
+    let current = ''
+    function addTodo(){
+        todos = [...todos,current]
+    }
+</script>
+<div>
+    <input
+        placeholder="enter something"
+        bind:value={current}
+    />
+    <button on:click={addTodo}>add</button>
+    {#each todos as todo,i}
+        <h4>{todo}</h4>
+    {/each}
+</div>
+
+<style>
+    input{
+        border: none;
+        font-size: 1rem;
+        background-color: rgb(235, 225, 225);
+        color: rgb(0, 0, 0);
+        padding: 1rem;
+        border-radius: 0.4rem;
+        font-size: 1.3rem;
+    }
+    input:focus{
+        outline: none;
+    }
+    button{
+        background-color: yellow;
+        font-size: 1.2rem;
+    }
+</style>
